115a48480fa8238a8adae71f4807a88a3306f730
Charles <charles.marttins@gmail.com>
2022-04-21 02:35:29 -0300

    Botão css

--- Java/Script.js
+++ Java/Script.js
@@ -1,7 +1,12 @@
+function botAdd(){
+   alert("Bora bora");
+}
+
 function getQuizzes() {
    let promise = axios.get("https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes");
    promise.then(console.log(promise));
 }
 
 getQuizzes();
+
 
