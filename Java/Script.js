9d72ea2aa55c8d3b226df6274d8ff5bace0b40aa
Charles <charles.marttins@gmail.com>
2022-04-26 15:52:00 -0300

    Envio final

--- Java/Script.js
+++ Java/Script.js
@@ -1,38 +1,187 @@
+const segundos = 1000;
+let novoQuizz = {
+    title: "",
+    imagem: "",
+    questoes: [],
+    niveis: [],
+}
+
+const errorTitle = "O titulo deve ter entre 20 e 60 caracteres"
+
 const API = "https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes";
 
+
+//Página inicial de criação do quizz!!
 const botAdd = () => {
-   let page = document.querySelector(".criacaoQuizz");
+    let page = document.querySelector(".criacaoQuizz");
    page.innerHTML =
    `
-   <h3> Comece pelo começo </h3>
+    <h4> Comece pelo começo </h4>
    <div class="quizzGeral">
- 
   <div class="titleQuizz" >
-  <input class="nameQ" name="title" type="text" placeholder="Titulo do seu quizz" focus>
+  <input class="boxQuizz tituloQuizz" name="title" type="text" placeholder="Titulo do seu quizz">
+  <span class="textoError hidden">O título do quizz deve ter entre 20 e 60 caracteres</span>
   </div>
   <div class="imageUrl">
- <input class="nameQ" name="title" type="text" placeholder="URL da imagem do seu quizz" focus>
+ <input class="boxQuizz valorImg" name="title" type="text" placeholder="URL da imagem do seu quizz">
+ <span class="textoError textoErrorImg hidden">A URL não é válida</span>
   </div>
   <div class="questionQtd">
-  <input class="nameQ" name="title" type="text" placeholder="Quantidade de perguntas do seu quizz" focus>
+  <input class="boxQuizz perguntasQuizz" name="title" type="number" placeholder="Quantidade de perguntas do seu quizz">
+  <span class="textoError textoErrorQues hidden">O quizz deve ter no minimo 3 perguntas</span>
   </div>
   <div class="levelsQtd">
-  <input class="nameQ" name="title" type="text" placeholder="Quantidades de níveis do seu quizz" focus>
+  <input class="boxQuizz niveisQuizz" name="title" type="number" placeholder="Quantidades de níveis do seu quizz">
+  <span class="textoError textoErrorLevels hidden">O quizz deve ter no minimo 2 níveis</span>
   </div>
-   
-   
    </div>
-   <button onclick="criarQuizz()" class="botaoPag1Quizz">
+   <button onclick="condicoesError()" class="botaoPag1Quizz">
    Prosseguir para criar perguntas
-   </button>
-    `
+   </button>`
+
+   
 }
 
-const criarQuizz = () => {
-    let pagCriação = document.querySelector(".container")
-    pagCriação.innerHTML = `<button>Ola</button>`
+
+const condicoesError = () => {
+let titleIni = "";
+let imgUrl = "";
+let questionsValor = "";
+let levelsValor = "";
+    let title = document.querySelector(".tituloQuizz").value;
+    
+    if(title.length > 20 && title.length < 60){
+      titleIni = false
+     
+    }
+    else {const errorBa = document.querySelector('.textoError');
+    errorBa.classList.remove("hidden")} 
+    
+
+    
+    const testUrl = "https://";
+    let valueUrl = document.querySelector(".valorImg").value;
+    
+
+    if (valueUrl.startsWith(testUrl)){
+     imgUrl = false
+       
+    }
+    else{
+        const errorUrl = document.querySelector('.textoErrorImg');
+ errorUrl.classList.remove("hidden")
+    }
+
+
+ let questions = document.querySelector(".perguntasQuizz").value;
+  if (questions >= 3 && questions <= 15){
+      questionsValor = false
+    
+ }
+ else {const errorQuest = document.querySelector('.textoErrorQues');
+ errorQuest.classList.remove("hidden")
+}
+
+let levels = document.querySelector(".niveisQuizz").value;
+if (levels >= 2 && levels <= 10 ){
+  levelsValor = false
+   
+}
+else {
+    const errorLevels = document.querySelector(".textoErrorLevels");
+    errorLevels.classList.remove("hidden")
+   
+}
+if (titleIni === false && imgUrl === false && questionsValor === false && levelsValor === false){
+    criarQuizz()
+}
+
+
+
+
+
+
 
 }
+
+
+
+
+
+
+function criarQuizz(questions) {
+    let inputQuestions = "";
+    for (i = 1; i <= questions; i++){
+        inputQuestions += `
+       `
+    }
+    
+   let testando = document.querySelector('.container')
+   testando.innerHTML =  `
+
+<div class="perguntasGeral">
+  <h4> Crie suas perguntas </h4>
+   <div class="perguntasOne">
+ <div class="PerguntaOne" >
+ <h5> Pergunta 1 </h5>
+ <input class="boxQuestions tituloQuestion" name="title" type="text" placeholder="Texto da pergunta" focus>
+ <span class="textoError hidden">A pergunta de ter no minimo 20 caracteres</span>
+ </div>
+ <div class="corDeFundoDaPergunta" >
+ <input class="boxQuestions corQuestion" name="title" type="text" placeholder="Cor de fundo da pergunta" focus>
+ <span class="textoError hidden">A cor deve ter o modelo hexadecimal</span>
+ </div>
+ <h5> Resposta correta </h5>
+ <div class="repostaCorreta" >
+ <input class="boxQuestions respostaQuestion" name="title" type="text" placeholder="Resposta correta" focus>
+ <span class="textoError hidden">Texto da resposta não pode ser vazio</span>
+ <div class="urlImage">
+ <input class="boxQuestions respostaQuestion" name="title" type="text" placeholder="URL da imagem" focus>
+ <span class="textoError hidden">A URL não é válida</span>
+ </div>
+ </div>
+ <h5> Respostas incorretas </h5>
+ <div class="repostaIncorreta1" >
+ <input class="boxQuestions respostaQuestion" name="title" type="text" placeholder="Resposta incorreta 1" focus>
+ <span class="textoError hidden">Texto da resposta não pode ser vazio</span>
+ <div class="urlImageIncorreta1">
+ <input class="boxQuestions respostaQuestion" name="title" type="text" placeholder="URL da imagem 1" focus>
+ <span class="textoError hidden">A URL não é válida</span>
+ </div>
+ <div class="repostaIncorreta2" >
+ <input class="boxQuestions respostaQuestion" name="title" type="text" placeholder="Resposta incorreta 2" focus>
+ <span class="textoError hidden">Texto da resposta não pode ser vazio</span>
+ <div class="urlImageIncorreta2">
+ <input class="boxQuestions respostaQuestion" name="title" type="text" placeholder="URL da imagem 2" focus>
+ <span class="textoError hidden">A URL não é válida</span>
+ </div>
+ <div class="repostaIncorreta3" >
+ <input class="boxQuestions respostaQuestion" name="title" type="text" placeholder="Resposta incorreta 3" focus>
+ <span class="textoError hidden">Texto da resposta não pode ser vazio</span>
+ <div class="urlImageIncorreta1">
+ <input class="boxQuestions respostaQuestion" name="title" type="text" placeholder="URL da imagem 3" focus>
+ <span class="textoError hidden">A URL não é válida</span>
+ </div>
+ </div>
+ 
+  </div>
+  </li>
+  <button onclick="condicoesError()" class="botaoPag1Quizz">
+  Prosseguir para criar perguntas
+  </button>`
+  
+}
+
+
+const criacaoNiveis = (questoes) => {
+
+let novaVida = document.querySelector("container");
+novaVida.innerHTML = `<div class"CriaN"
+<h2>Que mundo cruel>`
+}
+
+
+
 
 function getQuizzes() {
     let promise = axios.get(API);
